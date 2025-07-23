f65295fec19588adab3211965a54fcd8d1e69aff
MANEESHWARAN SARAVANAN <maneeshwaran01@gmail.com>
2025-07-23 14:48:11 +0530

    seo

--- smwportfolio/src/app/app.component.ts
+++ smwportfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { AboutComponent } from './page/about/about.component';
@@ -7,6 +7,7 @@ import { ServiceComponent } from './page/service/service.component';
 import { ProjectsComponent } from './page/projects/projects.component';
 import { ContactComponent } from './page/contact/contact.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -23,8 +24,31 @@ import { FooterComponent } from './shared/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Maneeshwaran | Portfolio';
+export class AppComponent implements OnInit{
+  // title = 'Maneeshwaran | Portfolio';
   // email: string = 'maneeshwaran01@gmail.com';
 
+  
+ constructor(private titleService: Title, private metaService: Meta) {}
+
+    ngOnInit(): void {
+    // Set page title
+    this.titleService.setTitle('Maneeshwaran | Fullstack Developer | UI/UX Designer');
+
+    // Add/update meta tags
+    this.metaService.addTags([
+      {
+        name: 'description',
+        content: "I'm Maneeshwaran, a Fullstack Developer skilled in Angular, Node.js, and Figma UI/UX Design. I build fast, modern web apps and elegant interfaces."
+      },
+      {
+        name: 'keywords',
+        content: 'Maneeshwaran, Fullstack Developer, Angular Developer, Node.js Developer, Figma UI UX Designer, Web Developer, Portfolio, Frontend, Backend'
+      },
+      {
+        name: 'author',
+        content: 'Maneeshwaran'
+      }
+    ]);
+  }
 }
