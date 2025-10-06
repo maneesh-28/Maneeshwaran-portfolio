91901f60945207e2c48c0332f1d9bb07a2090f34
MANEESHWARAN SARAVANAN <maneeshwaran01@gmail.com>
2025-10-06 19:16:15 +0530

    Add custom domain to CORS whitelist

--- smwportfolio/src/app/environments/environment.prod.ts
+++ smwportfolio/src/app/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://maneeshwaran-portfolio.onrender.com' // replace with actual Render URL
+  // apiUrl: 'https://maneeshwaran-portfolio.onrender.com' // replace with actual Render URL
+  apiUrl: 'https://maneeshwaran-portfolio.onrender.com'
+
 };
